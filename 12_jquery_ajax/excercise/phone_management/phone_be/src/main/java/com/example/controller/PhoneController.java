d2c5fc63ad26e2fba89d0f92a0df7ee2fe4185b0
minhtri1709 <trihoangminh52@gmail.com>
2022-09-30 13:33:42 +0700

    fix

--- 12_jquery_ajax/excercise/phone_management/phone_be/src/main/java/com/example/controller/PhoneController.java
+++ 12_jquery_ajax/excercise/phone_management/phone_be/src/main/java/com/example/controller/PhoneController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("phone/v1")
 @CrossOrigin
@@ -26,8 +28,25 @@ public class PhoneController {
         return new ResponseEntity<>(iPhoneService.save(phone),HttpStatus.OK);
     }
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Phone> showById(@PathVariable int id){
-//       return new ResponseEntity<Phone>(iPhoneService.findById(id),HttpStatus.OK);
-//    }
+    @GetMapping("/update/{id}")
+    public ResponseEntity<Phone> showById(@PathVariable int id){
+        Phone smartphone = iPhoneService.findById(id).get();
+        if (smartphone == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(smartphone, HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity saveUpdate(@PathVariable int id,@RequestBody Phone phone) {
+        Optional<Phone> findPhone = iPhoneService.findById(id);
+        if (!findPhone.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        findPhone.get().setProducer(phone.getProducer());
+        findPhone.get().setModel(phone.getModel());
+        findPhone.get().setPrice(phone.getPrice());
+        iPhoneService.save(findPhone.get());
+        return new ResponseEntity(findPhone, HttpStatus.OK);
+    }
 }
